cfce10e205a9b48c820f3f3a042d4fb999325305
Thourum <samoasvanyi@gmail.com>
2016-07-08 22:44:16 +0200

    Fix a bug where not valid id crashed the server

--- Server/api/users/handler/user/id/getUser.js
+++ Server/api/users/handler/user/id/getUser.js
@@ -2,12 +2,14 @@
 
 const Boom = require('boom');
 const User = require('../../../models/User');
+const Code = require(`${PATH}/config/errorCodes`);
 const formatUser = require('../../../util/userFunctions').formatUser;
 
 module.exports = (req, res) => {
   User.findById(req.params.id, function (err, user) {
-    if (err) {
-      res(Boom.badRequest(err));
+    if (err || !user) {
+      res(Code.userNotFound);
+      return;
     }
     res(formatUser(user, 'users')).code(200);
   });
