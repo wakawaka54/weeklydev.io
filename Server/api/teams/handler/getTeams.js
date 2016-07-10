bd03fed11048c59a4d69c9733817f65e76ac1062
Thourum <samoasvanyi@gmail.com>
2016-07-10 16:26:46 +0200

    Fixed populatin in getTeams

--- Server/api/teams/handler/getTeams.js
+++ Server/api/teams/handler/getTeams.js
@@ -4,7 +4,7 @@ const Boom = require('boom');
 const Team = require('../models/Team');
 
 module.exports = (req, res) => {
-  Team.find().populate('manager.user frontend.user backend.user', 'id username is_searching project team').exec((err, team) => {
+  Team.find().populate('manager frontend backend', 'id username is_searching project team').exec((err, team) => {
     res(team);
   });
 };
