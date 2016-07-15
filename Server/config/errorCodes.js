91c88ba636f2322f53f4b9563574cfc48c1353a2
Sagar Desai <sagarmonkeyboy@gmail.com>
2016-07-14 21:20:21 -0400

    Updated errorCodes.js to use es6 exports.

--- Server/config/errorCodes.js
+++ Server/config/errorCodes.js
@@ -14,23 +14,24 @@
 //  Submissions   :   7xx
 'use strict';
 
-const Boom = require('boom');
-function applyErrorCode (error) {
+import Boom from 'boom'
+
+export function applyErrorCode (error) {
   error.output.payload.errorCode = error.data.errorCode;
   return error;
 }
-module.exports = {
-  applyErrorCode: applyErrorCode,
-  // Server
-  internalServerError: applyErrorCode(Boom.create(500, 'Sorry, something went wrong on our end', {errorCode: '001'})),
-  // Users
-  userNotFound: applyErrorCode(Boom.create(404, 'User not found', {errorCode: 300})),
-  userInTooManyTeams: applyErrorCode(Boom.create(400, 'User in too many Teams', { errorCode: 305 })),
-  // Teams
-  teamNotFound: applyErrorCode(Boom.create(404, 'Team not found', { errorCode: 500})),
-  notOwner: applyErrorCode(Boom.create(401, 'You are not owner of this team', { errorCode: 501})),
-  userInTeam: applyErrorCode(Boom.create(400, 'User already on team', { errorCode: 502 })),
-  maxUsersInRole: applyErrorCode(Boom.create(400, 'Maximum users in role reached', { errorCode: 503 })),
-  maxRequestsReached: applyErrorCode(Boom.create(400, 'Maximum requests reached', { errorCode: 505 })),
-  alreadyRequested: applyErrorCode(Boom.create(400, 'You already requested to join this team', { errorCode: 506}))
-};
+
+// Server
+export const internalServerError = applyErrorCode(Boom.create(500, 'Sorry, something went wrong on our end', {errorCode: '001'}))
+
+// Users
+export const userNotFound = applyErrorCode(Boom.create(404, 'User not found', {errorCode: 300}))
+export const userInTooManyTeams = applyErrorCode(Boom.create(400, 'User in too many Teams', { errorCode: 305 }))
+
+// Teams
+export const teamNotFound = applyErrorCode(Boom.create(404, 'Team not found', { errorCode: 500}))
+export const notOwner = applyErrorCode(Boom.create(401, 'You are not owner of this team', { errorCode: 501}))
+export const userInTeam = applyErrorCode(Boom.create(400, 'User already on team', { errorCode: 502 }))
+export const maxUsersInRole = applyErrorCode(Boom.create(400, 'Maximum users in role reached', { errorCode: 503 }))
+export const maxRequestsReached = applyErrorCode(Boom.create(400, 'Maximum requests reached', { errorCode: 505 }))
+export const alreadyRequested = applyErrorCode(Boom.create(400, 'You already requested to join this team', { errorCode: 506}))
