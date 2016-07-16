3ca86b25edaba13ff03ab9c054ae5c9976747d6f
wb9688 <wbertrums@gmail.com>
2016-07-16 13:28:37 +0200

    Added Angular project

---
+++ Angular/conf/webpack.conf.js
@@ -0,0 +1,76 @@
+const webpack = require('webpack');
+const conf = require('./gulp.conf');
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  module: {
+    loaders: [
+      {
+        test: /.json$/,
+        loaders: [
+          'json'
+        ]
+      },
+      {
+        test: /\.(css|scss)$/,
+        loaders: [
+          'style',
+          'css',
+          'sass',
+          'postcss'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: [
+          'ng-annotate',
+          'babel'
+        ]
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        loaders: [
+          'url?limit=10000&mimetype=application/font-woff'
+        ]
+      },
+      {
+        test: /\.ttf$/,
+        loaders: [
+          'url?limit=10000&mimetype=application/octet-stream'
+        ]
+      },
+      {
+        test: /\.eot$/,
+        loaders: [
+          'file'
+        ]
+      },
+      {
+        test: /\.svg$/,
+        loaders: [
+          'url?limit=10000&mimetype=image/svg+xml'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      template: conf.path.src('index.html'),
+      inject: true
+    })
+  ],
+  postcss: () => [autoprefixer],
+  debug: true,
+  devtool: 'cheap-module-eval-source-map',
+  output: {
+    path: path.join(process.cwd(), conf.paths.tmp),
+    filename: 'index.js'
+  },
+  entry: `./${conf.path.src('index')}`
+};
